d03d1741c0508d79956176f2815ad97d2386546e
shilpisirohi12 <shilpisirohi12@gmail.com>
2019-03-20 23:28:32 -0400

    ``

--- Lab3/faceGoal.py
+++ Lab3/faceGoal.py
@@ -14,8 +14,43 @@
 
 import cv2 as cv
 import time
+import Adafruit_PCA9685
+import signal
+import math
 from ThreadedWebcam import ThreadedWebcam
 from UnthreadedWebcam import UnthreadedWebcam
+
+# The servo hat uses its own numbering scheme within the Adafruit library.
+# 0 represents the first servo, 1 for the second, and so on.
+LSERVO = 0
+RSERVO = 1
+
+
+# This function is called when Ctrl+C is pressed.
+# It's intended for properly exiting the program.
+def ctrlC(signum, frame):
+    print("Exiting")
+    # Stop the servos
+    pwm.set_pwm(LSERVO, 0, 0);
+    pwm.set_pwm(RSERVO, 0, 0);
+    exit()
+
+
+# Attach the Ctrl+C signal interrupt
+signal.signal(signal.SIGINT, ctrlC)
+
+
+# Initialize the servo hat library.
+pwm = Adafruit_PCA9685.PCA9685()
+
+# 50Hz is used for the frequency of the servos.
+pwm.set_pwm_freq(50)
+
+# Write an initial value of 1.5, which keeps the servos stopped.
+# Due to how servos work, and the design of the Adafruit library,
+# the value must be divided by 20 and multiplied by 4096.
+pwm.set_pwm(LSERVO, 0, math.floor(1.5 / 20 * 4096));
+pwm.set_pwm(RSERVO, 0, math.floor(1.5 / 20 * 4096));
 
 FPS_SMOOTHING = 0.9
 
