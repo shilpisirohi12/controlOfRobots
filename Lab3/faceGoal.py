276aadacf50e467912ec09d9ab30a71c150269b7
shilpisirohi12 <shilpisirohi12@gmail.com>
2019-03-20 23:21:05 -0400

    removing class

--- Lab3/faceGoal.py
+++ Lab3/faceGoal.py
@@ -15,139 +15,133 @@
 import cv2 as cv
 import time
 from ThreadedWebcam import ThreadedWebcam
+from UnthreadedWebcam import UnthreadedWebcam
+
+FPS_SMOOTHING = 0.9
+
+# Window names
+WINDOW1 = "Adjustable Mask - Press Esc to quit"
+WINDOW2 = "Detected Blobs - Press Esc to quit"
+
+# Default HSV ranges
+# Note: the range for hue is 0-180, not 0-255
+minH =   0; minS = 127; minV =   0;
+maxH = 180; maxS = 255; maxV = 255;
 
 
-class faceGoal(ThreadedWebcam):
+# These functions are called when the user moves a trackbar
+def onMinHTrackbar(val):
+    # Calculate a valid minimum red value and re-set the trackbar.
+    global minH
+    global maxH
+    minH = min(val, maxH - 1)
+    cv.setTrackbarPos("Min Hue", WINDOW1, minH)
 
-    # To do initialization
-    def __init__(self):
-        super().__init__()
-        self.FPS_SMOOTHING = 0.9
+def onMinSTrackbar(val):
+    global minS
+    global maxS
+    minS = min(val, maxS - 1)
+    cv.setTrackbarPos("Min Sat", WINDOW1, minS)
 
-        # Window names
-        self.WINDOW1 = "Adjustable Mask - Press Esc to quit"
-        self.WINDOW2 = "Detected Blobs - Press Esc to quit"
+def onMinVTrackbar(val):
+    global minV
+    global maxV
+    minV = min(val, maxV - 1)
+    cv.setTrackbarPos("Min Val", WINDOW1, minV)
 
-        # Default HSV ranges
-        # Note: the range for hue is 0-180, not 0-255
-        self.minH = 0; self.minS = 127; self.minV = 0;
-        self.maxH = 180; self.maxS = 255; self.maxV = 255;
+def onMaxHTrackbar(val):
+    global minH
+    global maxH
+    maxH = max(val, minH + 1)
+    cv.setTrackbarPos("Max Hue", WINDOW1, maxH)
 
-        # Initialize the threaded camera
-        # You can run the unthreaded camera instead by changing the line below.
-        # Look for any differences in frame rate and latency.
-        self.camera = ThreadedWebcam() # UnthreadedWebcam()
-        self.camera.start()
+def onMaxSTrackbar(val):
+    global minS
+    global maxS
+    maxS = max(val, minS + 1)
+    cv.setTrackbarPos("Max Sat", WINDOW1, maxS)
 
-        # Initialize the SimpleBlobDetector
-        self.params = cv.SimpleBlobDetector_Params()
-        self.detector = cv.SimpleBlobDetector_create(self.params)
-
-
-    # These functions are called when the user moves a trackbar
-    def onMinHTrackbar(self,val):
-        # Calculate a valid minimum red value and re-set the trackbar.
-
-        self.minH = min(val, self.maxH - 1)
-        cv.setTrackbarPos("Min Hue", self.WINDOW1, self.minH)
-
-    def onMinSTrackbar(self,val):
-
-        self.minS = min(val, self.maxS - 1)
-        cv.setTrackbarPos("Min Sat", self.WINDOW1, self.minS)
-
-    def onMinVTrackbar(self,val):
-
-        self.minV = min(val, self.maxV - 1)
-        cv.setTrackbarPos("Min Val", self.WINDOW1, self.minV)
-
-    def onMaxHTrackbar(self,val):
-
-        self.maxH = max(val, self.minH + 1)
-        cv.setTrackbarPos("Max Hue", self.WINDOW1, self.maxH)
-
-    def onMaxSTrackbar(self,val):
-
-        self.maxS = max(val, self.minS + 1)
-        cv.setTrackbarPos("Max Sat", self.WINDOW1, self.maxS)
-
-    def onMaxVTrackbar(self,val):
-
-        self.maxV = max(val, self.minV + 1)
-        cv.setTrackbarPos("Max Val", self.WINDOW1, self.maxV)
+def onMaxVTrackbar(val):
+    global minV
+    global maxV
+    maxV = max(val, minV + 1)
+    cv.setTrackbarPos("Max Val", WINDOW1, maxV)
 
 
-    def  blobDetect(self):
-        print("***inside Blob Detect***")
-        # Attempt to open a SimpleBlobDetector parameters file if it exists,
-        # Otherwise, one will be generated.
-        # These values WILL need to be adjusted for accurate and fast blob detection.
-        fs = cv.FileStorage("params.yaml", cv.FILE_STORAGE_READ); #yaml, xml, or json
-        if fs.isOpened():
-            self.detector.read(fs.root())
-        else:
-            print("WARNING: params file not found! Creating default file.")
+# Initialize the threaded camera
+# You can run the unthreaded camera instead by changing the line below.
+# Look for any differences in frame rate and latency.
+camera = ThreadedWebcam() # UnthreadedWebcam()
+camera.start()
 
-            fs2 = cv.FileStorage("params.yaml", cv.FILE_STORAGE_WRITE)
-            self.detector.write(fs2)
-            fs2.release()
+# Initialize the SimpleBlobDetector
+params = cv.SimpleBlobDetector_Params()
+detector = cv.SimpleBlobDetector_create(params)
 
-        fs.release()
+# Attempt to open a SimpleBlobDetector parameters file if it exists,
+# Otherwise, one will be generated.
+# These values WILL need to be adjusted for accurate and fast blob detection.
+fs = cv.FileStorage("params.yaml", cv.FILE_STORAGE_READ); #yaml, xml, or json
+if fs.isOpened():
+    detector.read(fs.root())
+else:
+    print("WARNING: params file not found! Creating default file.")
+    
+    fs2 = cv.FileStorage("params.yaml", cv.FILE_STORAGE_WRITE)
+    detector.write(fs2)
+    fs2.release()
+    
+fs.release()
 
-        # Create windows
-        cv.namedWindow(self.WINDOW1)
-        cv.namedWindow(self.WINDOW2)
+# Create windows
+cv.namedWindow(WINDOW1)
+cv.namedWindow(WINDOW2)
 
-        # Create trackbars
-        cv.createTrackbar("Min Hue", self.WINDOW1, self.minH, 180, self.onMinHTrackbar)
-        cv.createTrackbar("Max Hue", self.WINDOW1, self.maxH, 180, self.onMaxHTrackbar)
-        cv.createTrackbar("Min Sat", self.WINDOW1, self.minS, 255, self.onMinSTrackbar)
-        cv.createTrackbar("Max Sat", self.WINDOW1, self.maxS, 255, self.onMaxSTrackbar)
-        cv.createTrackbar("Min Val", self.WINDOW1, self.minV, 255, self.onMinVTrackbar)
-        cv.createTrackbar("Max Val", self.WINDOW1, self.maxV, 255, self.onMaxVTrackbar)
+# Create trackbars
+cv.createTrackbar("Min Hue", WINDOW1, minH, 180, onMinHTrackbar)
+cv.createTrackbar("Max Hue", WINDOW1, maxH, 180, onMaxHTrackbar)
+cv.createTrackbar("Min Sat", WINDOW1, minS, 255, onMinSTrackbar)
+cv.createTrackbar("Max Sat", WINDOW1, maxS, 255, onMaxSTrackbar)
+cv.createTrackbar("Min Val", WINDOW1, minV, 255, onMinVTrackbar)
+cv.createTrackbar("Max Val", WINDOW1, maxV, 255, onMaxVTrackbar)
 
-        fps, prev = 0.0, 0.0
-        while True:
-            # Calculate FPS
-            now = time.time()
-            print(now,"  ",prev);
-            fps = (fps*self.FPS_SMOOTHING + (1/(now - prev))*(1.0 - self.FPS_SMOOTHING))
-            prev = now
-            print("fps:",fps)
-            # Get a frame
-            frame = self.camera.read()
+fps, prev = 0.0, 0.0
+while True:
+    # Calculate FPS
+    now = time.time()
+    fps = (fps*FPS_SMOOTHING + (1/(now - prev))*(1.0 - FPS_SMOOTHING))
+    prev = now
 
-            # Blob detection works better in the HSV color space
-            # (than the RGB color space) so the frame is converted to HSV.
-            frame_hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
-            print("frame_hsv: ",frame_hsv)
+    # Get a frame
+    frame = camera.read()
+    
+    # Blob detection works better in the HSV color space 
+    # (than the RGB color space) so the frame is converted to HSV.
+    frame_hsv = cv.cvtColor(frame, cv.COLOR_BGR2HSV)
+    
+    # Create a mask using the given HSV range
+    mask = cv.inRange(frame_hsv, (minH, minS, minV), (maxH, maxS, maxV))
+    
+    # Run the SimpleBlobDetector on the mask.
+    # The results are stored in a vector of 'KeyPoint' objects,
+    # which describe the location and size of the blobs.
+    keypoints = detector.detect(mask)
+    
+    # For each detected blob, draw a circle on the frame
+    frame_with_keypoints = cv.drawKeypoints(frame, keypoints, None, color = (0, 255, 0), flags = cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
+    
+    # Write text onto the frame
+    cv.putText(frame_with_keypoints, "FPS: {:.1f}".format(fps), (5, 15), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0))
+    cv.putText(frame_with_keypoints, "{} blobs".format(len(keypoints)), (5, 35), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0))
+    
+    # Display the frame
+    cv.imshow(WINDOW1, mask)
+    cv.imshow(WINDOW2, frame_with_keypoints)
+    
+    # Check for user input
+    c = cv.waitKey(1)
+    if c == 27 or c == ord('q') or c == ord('Q'): # Esc or Q
+        camera.stop()
+        break
 
-            # Create a mask using the given HSV range
-            mask = cv.inRange(frame_hsv, (self.minH, self.minS, self.minV), (self.maxH, self.maxS, self.maxV))
-            print("mask: ",mask)
-            # Run the SimpleBlobDetector on the mask.
-            # The results are stored in a vector of 'KeyPoint' objects,
-            # which describe the location and size of the blobs.
-            keypoints = self.detector.detect(mask)
-
-            # For each detected blob, draw a circle on the frame
-            frame_with_keypoints = cv.drawKeypoints(frame, keypoints, None, color = (0, 255, 0), flags = cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-            # Write text onto the frame
-            cv.putText(frame_with_keypoints, "FPS: {:.1f}".format(fps), (5, 15), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0))
-            cv.putText(frame_with_keypoints, "{} blobs".format(len(keypoints)), (5, 35), cv.FONT_HERSHEY_SIMPLEX, 0.5, (0,255,0))
-
-            # Display the frame
-            cv.imshow(self.WINDOW1, mask)
-            #cv.imshow(self.WINDOW2, frame_with_keypoints)
-
-            # Check for user input
-            c = cv.waitKey(1)
-            if c == 27 or c == ord('q') or c == ord('Q'): # Esc or Q
-                self.camera.stop()
-                break
-
-        self.camera.stop()
-
-obj=faceGoal()
-obj.blobDetect()
+#camera.stop()
